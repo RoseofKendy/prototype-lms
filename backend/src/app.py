fad72751d42a0d81f22dc5d3233cbe8ab14f7b1f
Sharon Kendi <sharonkendi35@gmail.com>
2026-03-21 12:55:12 +0300

    Configure flask database

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from src.config.db import db
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lms_user:password123@localhost/lms_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     @app.route("/")
     def home():
